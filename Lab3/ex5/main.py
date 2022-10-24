77659740ecda1a06dce2ea56353edbb7e8abc9e9
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-10-24 14:32:37 +0300

    tema

--- Lab3/ex5/main.py
+++ Lab3/ex5/main.py
@@ -1,9 +1,28 @@
 def test_dictionary(my_set, d):
-    # for arg in my_set:
-    #     if arg is not None:
-    #         print(d.setdefault(arg[0], 1))
-    #         if arg[1] in d.setdefault(arg[0], 1):
-    #
+    my_keys = []
+    for arg in my_set:
+        my_keys.append(arg[0])
+    for arg in d.keys():
+        if arg not in my_keys:
+            return 0
+    ok = 1
+    for arg in my_set:
+        val_d = d.setdefault(arg[0], None)
+        if val_d is not None:
+            if arg[1] != val_d[:len(arg[1])]:
+                ok = 0
+            mdd = len(val_d) - len(arg[3])
+            if arg[2] not in val_d[len(arg[1]):mdd]:
+                ok = 0
+            if arg[3] != val_d[mdd:]:
+                ok = 0
+        if ok == 1 and arg[0] is not None:
+            print(arg[0])
+    if ok:
+        return 1
+    else:
+        return 0
+
 
 
 def main():
