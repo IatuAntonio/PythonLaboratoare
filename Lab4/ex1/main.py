6e394b39f49d8355457fda2b8dabd40cf6e8bdc9
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-03 20:17:45 +0200

    lab4

---
+++ Lab4/ex1/main.py
@@ -0,0 +1,22 @@
+import os
+
+
+def file_extension(my_path):
+    os.chdir(my_path)
+    my_extensions = []
+    for file in os.listdir():
+        # print(file)
+        split_tup = os.path.splitext(file)
+        fe = split_tup[-1]
+        my_extensions.append(fe)
+
+    print(my_extensions)
+
+
+def main():
+    my_path = "D:\\Multe chestii\\Antonio\\Facultate\\Python\\PythonLaboratoare\\Lab4\\ex1\\myFolder"
+    file_extension(my_path)
+
+
+if __name__ == "__main__":
+    main()
