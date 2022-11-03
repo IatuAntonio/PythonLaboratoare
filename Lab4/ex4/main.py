e12775634b972c532f8cd8dd3eeccd343b8c92cb
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-03 22:24:05 +0200

    lab4

--- Lab4/ex4/main.py
+++ Lab4/ex4/main.py
@@ -10,13 +10,13 @@ def get_extensions(my_path):
     #     fe = split_tup[-1]
     #     if fe != "":
     #         extensions.add(fe)
+    # sorted(extensions)
     # print(extensions)
 
     # adaug extensiile care se regasesc doar o data
 
     extensions = []
     test = set()
-
     for file in os.listdir(my_path):
         split_tup = os.path.splitext(file)
         fe = split_tup[-1]
@@ -24,12 +24,13 @@ def get_extensions(my_path):
             extensions.append(fe)
             test.add(fe)
 
-    my_ext = set()
+    my_ext = []
     for i in test:
         if i in extensions:
             extensions.remove(i)
         if i not in extensions:
-            my_ext.add(i)
+            my_ext.append(i)
+    my_ext.sort()
     print(my_ext)
 
 
