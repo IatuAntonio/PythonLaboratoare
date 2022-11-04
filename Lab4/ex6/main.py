f679e25016a77e94d6390f72a388e928647c4871
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-04 10:26:27 +0200

    lab4

--- Lab4/ex6/main.py
+++ Lab4/ex6/main.py
@@ -3,19 +3,24 @@ import os
 files_list = []
 
 
-def callback():
-    # revin mai incolo
-    print("ceva")
+def callback_fnc(target):
+    try:
+        if "txt" not in target and not os.path.isdir(target):
+            raise Exception("Fisierul nu este text")
+    except Exception as e:
+        print("Fisierul nu este un document text")
 
 
-def find_string(target, to_search):
+def find_string(target, to_search, callback):
     # print(target)
-    if "txt" not in target and not os.path.isdir(target):
-        raise Exception("Fisierul nu este text")
+    # if "txt" not in target and not os.path.isdir(target):
+
     if os.path.isdir(target):
         for file in os.listdir(target):
             # os.chdir(target)
-            find_string(os.path.join(target, file), to_search)
+            find_string(os.path.join(target, file), to_search, callback)
+
+    callback(target)
 
     if not os.path.isdir(target) and "txt" in target:
         my_file = target.split("\\")
@@ -33,8 +38,8 @@ def find_string(target, to_search):
 
 
 def main():
-    my_path = "myFolder - Copy"
-    print(find_string(my_path, "ceva"))
+    my_path = "myFolder"
+    print(find_string(my_path, "ceva", callback_fnc))
 
 
 if __name__ == "__main__":
