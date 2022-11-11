7c5c5dfd63b62684ad46c0aebf7d18c136c049af
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-11 10:17:27 +0200

    lab5

--- Lab5/ex7/main.py
+++ Lab5/ex7/main.py
@@ -2,46 +2,35 @@ def sum_digits(x):
     return sum(map(int, str(x)))
 
 
-def fibo(nr):
-    if nr == 0:
-        return 0
-    elif nr == 1:
-        return 1
-    else:
-        return fibo(nr-1) + fibo(nr-2)
+def fibo():
+    a = 0
+    b = 1
+    fibo_list = [a, b]
+    for i in range(1, 100):
+        c = a + b
+        fibo_list.append(c)
+        a = b
+        b = c
+    return fibo_list
 
 
 def process(**args):
-    fibo_list = []
-    right_list = []
-    # revin
-    # for i in range(0, 1001):
-    #     fibo_list.append(fibo(i))
-    #
-    # for element in args:
-    #     if element == "filters":
-    #         ceva = args.values()
-    #         ceva(fibo_list)
-    #
-    # for element in args:
-    #     if element == "offset":
-    #         cnt = args.values()
-    #         while cnt:
-    #             fibo_list.pop(0)
-    #             cnt -= 1
-    #
-    # for element in args:
-    #     if element == "limit":
-    #         cnt =
-    #         cnt = int(cnt)
-    #
-    #         while cnt:
-    #             right_list.append(fibo_list[cnt-1])
-    #             fibo_list.pop(0)
-    #             cnt -= 1
+    my_list = []
+    if "filters" in args.keys():
+        my_list = list([el for el in fibo() if args["filters"][0](el) == True])
+
+    if "offset" in args.keys():
+        my_list = my_list[args["offset"]+1:]
+
+    if "limit" in args.keys():
+        my_list = my_list[:args["limit"]]
+    print(my_list)
+    return my_list
+
 
 def main():
-    process(filters=[lambda item: item % 2 == 0, lambda item: item == 2 or 4 <= sum_digits(item) <= 20],   limit=2, offset = 2)
+    process(filters=[lambda item: item % 2 == 0, lambda item: item == 2 or 4 <= sum_digits(item) <= 20], limit=2,
+            offset=2)
 
 
 if __name__ == "__main__":
---
+++ Lab5/ex8/main.py
@@ -0,0 +1,10 @@
+def print_arguments(function):
+
+    # return f"Arguments are: ({param1}, {param2}), {} will return {val}"
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
