c41669f11215adbda17f4670341a6a54cca697cd
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-11 10:29:12 +0200

    lab5

--- Lab5/ex8/main.py
+++ Lab5/ex8/main.py
@@ -1,9 +1,25 @@
-def print_arguments(function):
+def multiply_by_two(x):
+    return x * 2
 
-    # return f"Arguments are: ({param1}, {param2}), {} will return {val}"
+
+def add_numbers(a, b):
+    return a + b
+
+
+def print_arguments(function):
+    def fnc(*args1, **args2):
+        val = function(*args1, **args2)
+        print(f"Arguments are: {args1}, {args2} will return {val}")
+        return val
+    return fnc
 
 
 def main():
+    augmented_multiply_by_two = print_arguments(multiply_by_two)
+    x = augmented_multiply_by_two(10)
+
+    augmented_add_numbers = print_arguments(add_numbers)
+    x = augmented_add_numbers(3, 4)
 
 
 if __name__ == "__main__":
