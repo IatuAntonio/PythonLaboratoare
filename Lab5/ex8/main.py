9bd5b3fe18e5e209aa5979c74aa16b413559ec49
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-11 10:34:22 +0200

    lab5

--- Lab5/ex8/main.py
+++ Lab5/ex8/main.py
@@ -14,12 +14,27 @@ def print_arguments(function):
     return fnc
 
 
+def multiply_by_three(x):
+    return x * 3
+
+
+def multiply_output(function):
+    def fnc(*args1, **args2):
+        val = function(*args1, **args2)
+        print(val)
+        return 2 * val
+    return fnc
+
+
 def main():
     augmented_multiply_by_two = print_arguments(multiply_by_two)
     x = augmented_multiply_by_two(10)
 
     augmented_add_numbers = print_arguments(add_numbers)
     x = augmented_add_numbers(3, 4)
+
+    augmented_multiply_by_three = multiply_output(multiply_by_three)
+    x = augmented_multiply_by_three(10)
 
 
 if __name__ == "__main__":
