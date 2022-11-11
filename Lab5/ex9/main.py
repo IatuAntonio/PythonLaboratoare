3c4a918172a785438a0e3125acea1d82ab7675a5
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-11 10:42:30 +0200

    lab5

--- Lab5/ex9/main.py
+++ Lab5/ex9/main.py
@@ -1,1 +1,21 @@
-def main()
+def fnc(pairs):
+    my_list = []
+
+    for pair in pairs:
+        # print(pair)
+        d = dict()
+        d["sum"] = pair[0] + pair[1]
+        d["prod"] = pair[0] * pair[1]
+        d["pow"] = pair[0] ** pair[1]
+        my_list.append(d)
+
+    print(my_list)
+    return my_list
+
+
+def main():
+    fnc(pairs=[(5, 2), (19, 1), (30, 6), (2, 2)])
+
+
+if __name__ == "__main__":
+    main()
