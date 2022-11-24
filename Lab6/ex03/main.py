acf3705b7828f2af884914ede4634f1e38b51948
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-24 20:48:53 +0200

    lab6

---
+++ Lab6/ex03/main.py
@@ -0,0 +1,20 @@
+import re
+
+
+def my_fnc(text, reg_text):
+    my_list = []
+    for element in text:
+        print(element)
+        ceva = re.findall(reg_text, element)
+        my_list.append(ceva)
+
+    print(my_list)
+
+
+def main():
+    my_list = ["ana 55", "dsad 787 dasffg 987", "gdysagud fdfds   fsd f sd ", "4565 534 fdgfd"]
+    my_fnc(my_list, r"[0-9]{3}")
+
+
+if __name__ == "__main__":
+    main()
