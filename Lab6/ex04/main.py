1a2f528d491c5afc76326bbd1fbca4635967ae75
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-25 10:08:38 +0200

    lab6

--- Lab6/ex04/main.py
+++ Lab6/ex04/main.py
@@ -1,14 +1,25 @@
 import re
 
 
-def my_fnc():
-
-
+def my_fnc(my_path, attrs):
+    my_list = []
+    fd = open(my_path, "r")
+    txt = fd.readlines()
+    for element in txt:
+        cnt = 0
+        for atr in attrs.items():
+            if re.search(atr[0]+"\s*=\s*\""+atr[1]+"\"", element):
+                cnt += 1
+        if cnt == len(attrs):
+            my_list.append(element)
+    for el in my_list:
+        print(el)
 
 
 def main():
-    my_fnc()
+    my_path = "D:\\Multe chestii\\Antonio\\Facultate\\Python\\PythonLaboratoare\\Lab6\\ex04\\file.txt"
+    my_fnc(my_path, {"class": "uncd", "url": "google.com", "name": "cd"})
 
 
-if __name__ == "__main__:":
+if __name__ == "__main__":
     main()
--- Lab6/ex05/main.py
+++ Lab6/ex05/main.py
@@ -1,10 +1,24 @@
 import re
 
 
-def my_fnc():
+def my_fnc(my_path, attrs):
+    my_list = []
+    fd = open(my_path, "r")
+    txt = fd.readlines()
+    for element in txt:
+        cnt = 0
+        for atr in attrs.items():
+            if re.search(atr[0]+"\s*=\s*\""+atr[1]+"\"", element):
+                cnt += 1
+        if cnt != 0:
+            my_list.append(element)
+    for el in my_list:
+        print(el)
 
 
 def main():
+    my_path = "D:\\Multe chestii\\Antonio\\Facultate\\Python\\PythonLaboratoare\\Lab6\\ex05\\file.txt"
+    my_fnc(my_path, {"class": "planta", "url": "google.com", "name": "cd"})
 
 
 if __name__ == "__main__":
