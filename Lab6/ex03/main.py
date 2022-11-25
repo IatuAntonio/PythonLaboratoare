754b3a6f4fae587ce328f757ea0270ca6b179153
IatuAntonio <79212418+IatuAntonio@users.noreply.github.com>
2022-11-25 10:12:44 +0200

    lab6

--- Lab6/ex03/main.py
+++ Lab6/ex03/main.py
@@ -4,15 +4,16 @@ import re
 def my_fnc(text, reg_text):
     my_list = []
     for element in text:
-        ceva = re.findall(reg_text, element)
-        my_list.append(ceva)
+        for rgex in reg_text:
+            ceva = re.findall(rgex, element)
+            my_list.append(ceva)
 
     print(my_list)
 
 
 def main():
     my_list = ["ana 55", "dsad 787 dasffg 987", "gdysagud fdfds   fsd f sd ", "4565 534 fdgfd"]
-    my_fnc(my_list, r"[0-9]{3}")
+    my_fnc(my_list, [r"[0-9]{3}", r"\w{3}"])
 
 
 if __name__ == "__main__":
